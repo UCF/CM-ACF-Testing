9f0da08580d0f49bfd64091d37d7fcfdf2fbb147
Jo Dickson <jogreybill@gmail.com>
2018-08-31 08:40:05 -0400

    Fixed warnings/notices

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Handle all theme configuration here
+ **/
+define( 'ADMISSIONS_THEME_URL', get_stylesheet_directory_uri() );
+define( 'ADMISSIONS_THEME_STATIC_URL', ADMISSIONS_THEME_URL . '/static' );
+define( 'ADMISSIONS_THEME_CSS_URL', ADMISSIONS_THEME_STATIC_URL . '/css' );
+define( 'ADMISSIONS_THEME_JS_URL', ADMISSIONS_THEME_STATIC_URL . '/js' );
+define( 'ADMISSIONS_THEME_IMG_URL', ADMISSIONS_THEME_STATIC_URL . '/img' );
+define( 'ADMISSIONS_THEME_CUSTOMIZER_PREFIX', 'admissions_' );
